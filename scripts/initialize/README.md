c6c1800b8d47297465845907da8256da75a3edff
Sasha Koss <koss@nocorp.me>
2019-08-30 11:36:32 +0200

    Implement install.sh script

---
+++ scripts/initialize/README.md
@@ -0,0 +1,16 @@
+# How to start with Firebun
+
+To start a Firebun project, run this command in your terminal:
+
+```bash
+curl git.io/fjxEB | sh -s project-name
+```
+
+_See [the source code of the script on GitHub](https://git.io/fjxMW)_
+
+Or you can use [firebun-app as a GitHub repo template](https://git.io/fjxME)
+and then run:
+
+```bash
+make start
+```
--- scripts/initialize/install.sh
+++ scripts/initialize/install.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-echo "It works!"
+set -e
+
+PROJECT_DIR=$1
+
+if [ -z "$PROJECT_DIR" ]
+then
+  echo "(•̀o•́)ง Project directory is missing! Please pass directory name as the first argument, e.g.: "
+  echo "  curl git.io/fjxEB | sh -s project-name"
+  exit 1
+fi
+
+echo "─=≡Σ((( つ◕ل͜◕)つ Creating Firebun project in ./$PROJECT_DIR"
+
+git clone --origin template https://github.com/kossnocorp/firebun-app.git $PROJECT_DIR
+cd $PROJECT_DIR
+
+make start
