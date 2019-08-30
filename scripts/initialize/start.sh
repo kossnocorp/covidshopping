88f5ea5bbd83f3d8fb471dd6a04a485c41117732
Sasha Koss <koss@nocorp.me>
2019-08-30 12:18:34 +0200

    Try to solve the interactive prompt problem

--- scripts/initialize/start.sh
+++ scripts/initialize/start.sh
@@ -5,6 +5,7 @@ set -e
 echo "(~‾▿‾)~ Initializing your application for the first time"
 
 echo "( ͡° ͜ʖ ͡°) It's settings time!"
+exec < /dev/tty;
 ts-node ./scripts/initialize/questions.ts
 
 echo "\( ﾟヮﾟ)/ It's ready!"
