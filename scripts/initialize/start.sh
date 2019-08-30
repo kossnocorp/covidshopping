e57b438194a81d7bfbf33410a20fc3ce842143e4
Sasha Koss <koss@nocorp.me>
2019-08-30 15:44:39 +0200

    Mess with git in the initialize script

--- scripts/initialize/start.sh
+++ scripts/initialize/start.sh
@@ -7,5 +7,13 @@ echo "(~‾▿‾)~ Initializing your application for the first time"
 echo "( ͡° ͜ʖ ͡°) It's settings time!"
 ts-node ./scripts/initialize/questions.ts
 
-echo "\( ﾟヮﾟ)/ It's ready!"
+# Remove the scripts, so this script won't run on make start
 rm -rf scripts/initialize
+
+echo "(｡◕‿‿◕｡) Creating git commit with the initial config"
+git stash
+git add scripts/initialize bun.config.ts
+git commit -c 'ᕕ( ᐛ )ᕗ Initialize the app'
+git stash pop
+
+echo "\( ﾟヮﾟ)/ It's ready!"
